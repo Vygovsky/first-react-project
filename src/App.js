9cff00141c60771e7e23fadc021e6450cb71df1f
Roman_v <vigmol@ukr.net>
2019-08-02 15:47:09 +0300

    add map for array

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Header from "./components/Header/Header";
+import Nav from "./components/Navbar/Nav";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+const App = () => {
+    return (<div className="app-wrapper">
+        <BrowserRouter>
+            <Nav/>
+            <Header/>
+            <div className="app-wrapper-content">
+                <Route  path={'/dialogs'} component={Dialogs}/>
+                <Route path={'/profile'} component={Profile}/>
+            </div>
+        </BrowserRouter>
+
+    </div>);
 }
-
 export default App;
